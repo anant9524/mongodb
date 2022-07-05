e61da4948fd200e0bbd7fc6d1872b9372c767d3e
anant9524 <86129870+anant9524@users.noreply.github.com>
2022-07-05 19:11:21 +0530

    Create mongo-primary.sh

---
+++ mongo-primary.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+echo "$(tput setaf 1)Installing mongoDB repo"
+tput setaf 7
+sudo wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+sudo echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+echo "$(tput setaf 1)Installing binaries"
+tput setaf 7
+sudo apt-get update
+echo "$(tput setaf 1)Installing mongodb service"
+tput setaf 7
+sudo apt-get install -y mongodb-org
+sleep 5
+echo "$(tput setaf 1)Enabling mongodb service to auto start on reboot"
+tput setaf 7
+sudo systemctl enable mongod
+sudo systemctl start mongod
+sleep 5
+echo "$(tput setaf 1)Creating admin user"
+tput setaf 7
+mongo <<'EOF'
+use admin
+db.createUser({user: "admin", pwd: "password", roles:[{role: "root", db: "admin"}]})
+exit
+EOF
+echo "$(tput setaf 1)Creating mongo key"
+tput setaf 7
+openssl rand -base64 756 > /opt/mongo-keyfile
+chmod 400 /opt/mongo-keyfile
+chown mongodb:mongodb /opt/mongo-keyfile
+echo "$(</opt/mongo-keyfile)"
+echo "$(tput setaf 1)copy this key and paste to other servers"
+tput setaf 7
+echo "$(tput setaf 2)editing conf file"
+tput setaf 7
+file=/etc/mongod.conf
+bindIp=$1
+replSetName=$2
+sed -i 's/#security/security/g' $file
+sed -i 's/security:/security: \n  keyFile: \/opt\/mongo-keyfile /g' $file
+sed -i 's/#replication/replication/g' $file
+sed -i 's/replication:/replication: \n  replSetName: /g' $file
+sed -i 's/bindIp:/bindIp: /g' $file
+sed -i 's/bindIp: .*/bindIp: '$bindIp'/' $file
+sed -i 's/replSetName:/replSetName: /g' $file
+sed -i 's/replSetName:.*/replSetName:'$replSetName'/' $file
+echo "$(tput setaf 2)restarting mongo service"
+tput setaf 7
+systemctl restart mongod
+sleep 5
